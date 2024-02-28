025545467794dc5632208980e9cfd46a9d0e8b20
saviganga A <saviour@mytruq.com>
2024-02-28 10:00:36 +0100

    feat: terraform: read key pair from local

--- devops/terraform/key_pair.tf
+++ devops/terraform/key_pair.tf
@@ -1,16 +1,9 @@
 # provision key 
-
-
-data "aws_s3_object" "public_key_object" {
-  bucket = "musipedia-secretes"
-  key    = "mb.pub"
-}
-
-
 resource "aws_key_pair" "musipedia-backend-key" {
   key_name   = var.KEY_PAIR_NAME
-  public_key = data.aws_s3_object.public_key_object.body
+  public_key = file("${path.module}/pub-keys/${var.PUBLIC_KEY}")
 }
+
 
 # print out some output
 output "key_name" {

e9f0f6458ff83e9edfc99891ff8cdbe4af12aee9
saviganga A <saviour@mytruq.com>
2024-02-28 14:40:33 +0100

    try deregistering ami

--- devops/terraform/instance.tf
+++ devops/terraform/instance.tf
@@ -10,6 +10,28 @@ data "aws_ami" "musipedia" {
   }
 }
 
+data "aws_ami" "deregister-list" {
+  most_recent      = false
+  owners           = ["self"]
+
+  filter {
+    name   = "tag:Name"
+    values = ["${var.PROJECT_NAME}-*"]
+  }
+}
+
+
+resource "aws_ami" "deregister" {
+  count = length(data.aws_ami.deregister-list.ids)
+
+  # Specify the ID of the AMI to deregister
+  image_id = data.aws_ami.deregister-list.ids[count.index]
+
+  # Set delete_on_termination to true to deregister the AMI
+  delete_on_termination = true
+}
+
+
 resource "aws_launch_template" "ec2-tpl" {
   name = "ec2-tpl"
 
@@ -37,5 +59,16 @@ resource "aws_launch_template" "ec2-tpl" {
 
 output "ami_output" {
   value = data.aws_ami.musipedia.name
+}
+
+
+
+output "deregister_ami_list_output" {
+  value = data.aws_ami.deregister-list
+}
+
+
+output "deregister_ami_output" {
+  value = data.aws_ami.deregister
 }
 

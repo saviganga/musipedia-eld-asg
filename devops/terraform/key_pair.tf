de2dd5ae241776ea9306c77c87f05563c30b2f59
saviganga A <saviour@mytruq.com>
2024-02-28 09:41:01 +0100

    feat: terraform: read key pair public key from s3

--- devops/terraform/key_pair.tf
+++ devops/terraform/key_pair.tf
@@ -12,12 +12,6 @@ resource "aws_key_pair" "musipedia-backend-key" {
   public_key = data.aws_s3_object.public_key_object.body
 }
 
-# resource "aws_key_pair" "musipedia-backend-key" {
-#   key_name   = var.KEY_PAIR_NAME
-#   public_key = file("${path.module}/pub-keys/${var.PUBLIC_KEY}")
-# }
-
-
 # print out some output
 output "key_name" {
   value = aws_key_pair.musipedia-backend-key.key_name

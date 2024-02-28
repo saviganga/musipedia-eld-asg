2c738bf5d7df2f41e60f48a47439f60ac515dd39
saviganga A <saviour@mytruq.com>
2024-02-28 08:38:09 +0100

    feat: upload terraform state file to s3

--- devops/terraform/providers.tf
+++ devops/terraform/providers.tf
@@ -10,8 +10,8 @@ terraform {
   }
 
   backend "s3" {
-    bucket         = "rs-terraform-statefile01"
-    key            = "terraform-statefile"
+    bucket         = "tf-backend-ganga"
+    key            = "terraform.tfstate"
     region         = "us-east-1"
   }
 }
